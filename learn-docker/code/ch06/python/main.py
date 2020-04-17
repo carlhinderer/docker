e19c4c1b6a7de74757db66930deeb5707d50c29b
Carl Hinderer <carl.hinderer4@gmail.com>
2020-04-17 13:59:42 -0600

    Added example of auto-restarting Python application.

--- learn-docker/code/ch06/python/main.py
+++ learn-docker/code/ch06/python/main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
  
 @app.route("/")
 def hello():
   return "Hello World!"
+ 
+@app.route("/colors")
+def colors():
+   return jsonify(["red", "green", "blue", "orange"])
  
 if __name__ == "__main__":
   app.run()
