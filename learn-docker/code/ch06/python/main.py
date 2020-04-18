832dc8991e33ad7bcdc5d10a8b2759d5e9c9fab6
Carl Hinderer <carl.hinderer4@gmail.com>
2020-04-17 18:14:39 -0600

    Added example of instrumenting a Python application.

--- learn-docker/code/ch06/python/main.py
+++ learn-docker/code/ch06/python/main.py
@@ -1,13 +1,28 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
+import logging
+
+
+logger = logging.getLogger("Sample App")
+logger.setLevel(logging.WARN)
+
+# Create a console handler (StreamHandler outputs to STDOUT)
+ch = logging.StreamHandler()
+
+# Create a formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
  
 @app.route("/")
 def hello():
-  return "Hello World!"
+    logger.info("Accessing endpoint '/'")
+    return "Hello World!"
  
 @app.route("/colors")
 def colors():
-   return jsonify(["red", "green", "blue", "orange"])
+    logger.warning("Warning, you are accessing /colors")
+    return jsonify(["red", "green", "blue", "orange"])
  
 if __name__ == "__main__":
   app.run()
