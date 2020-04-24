bfef3b1ef4e26e559238438aeb4b4ea71c9f36f2
Carl Hinderer <carl.hinderer4@gmail.com>
2020-04-23 20:28:43 -0600

    Added pipeline script.

---
+++ learn-docker/code/ch08/builder/pipeline.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# *** Sample script to build, test and push containerized Node.js applications ***
+
+# build the Docker image
+docker image build -t $HUB_USER/$REPOSITORY:$TAG .
+
+# Run all unit tests
+docker container run $HUB_USER/$REPOSITORY:$TAG npm test
+
+# Login to Docker Hub
+docker login -u $HUB_USER -p $HUB_PWD
+
+# Push the image to Docker Hub
+docker image push $HUB_USER/$REPOSITORY:$TAG
