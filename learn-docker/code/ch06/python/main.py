02c9b16d235e73c10b355a23ff77d54e21ded093
Carl Hinderer <carl.hinderer4@gmail.com>
2020-11-12 20:12:56 -0700

    Added dev Dockerfile for auto-reloading Flask app.

--- learn-docker/code/ch06/python/main.py
+++ learn-docker/code/ch06/python/main.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World'
+
+@app.route('/colors')
+def colors():
+    return jsonify(['red', 'green', 'blue'])
 
 if __name__ == '__main__':
     app.run()
