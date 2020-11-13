a531fd4f5d45d8d0fc7efc09c2d85e8d5cffe0c7
Carl Hinderer <carl.hinderer4@gmail.com>
2020-11-13 08:55:13 -0700

    Added logging to sample Python application.

--- learn-docker/code/ch06/python/main.py
+++ learn-docker/code/ch06/python/main.py
@@ -1,14 +1,30 @@
 from flask import Flask
 from flask import jsonify
+import logging
 
 app = Flask(__name__)
 
+# Create logger
+logger = logging.getLogger("Sample App")
+logger.setLevel(logging.WARN)
+
+# Create a console handler (StreamHandler outputs to STDOUT)
+ch = logging.StreamHandler()
+
+# Create a formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
 @app.route('/')
 def hello():
+    logger.info("Accessing endpoint '/'")
     return 'Hello World'
 
 @app.route('/colors')
 def colors():
+    logger.warning("Warning, you are accessing '/colors'")
     return jsonify(['red', 'green', 'blue'])
 
 if __name__ == '__main__':
